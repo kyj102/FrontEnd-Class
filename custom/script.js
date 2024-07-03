8f3c41f6290caf7cfed6420c8722c2713d8fe785
kyj102 <yain4746@naver.com>
2024-07-03 18:28:17 +0900

    custom

--- custom/script.js
+++ custom/script.js
@@ -3,3 +3,7 @@ const bgImgs = ["bg1.jpg", "bg2.jpg", "bg3.jpg", "bg4.jpg", "bg5.jpg"];
 const bgImg = document.querySelector("#background_img");
 console.log(bgImg);
 bgImg.style.backgroundImage = `radial-gradient(circle, transparent, rgba(0, 0, 0, 0.7)), url(./img2/${bgImgs[0]})`;
+
+const topContents = document.querySelector("#top_contents");
+const contentTit = topContents.querySelector(".top_contents_title");
+const contentDesc = topContents.querySelector(".top_contents_decs");
