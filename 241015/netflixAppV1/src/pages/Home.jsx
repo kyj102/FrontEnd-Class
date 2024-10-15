f1f255f81335aa63fc0147b19b9f0e82111dcb2e
kyj102 <yain4746@naver.com>
2024-10-15 16:52:27 +0900

    API를 이용한 redux 활용예제

---
+++ 241015/netflixAppV1/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { movieAction } from "../redux/actions/moiveAction";
+import Banner from "../components/Banner";
+const Home = () => {
+  const dispatch = useDispatch();
+  const { popularMovie, topRatedMovie, upComingMovie } = useSelector(
+    (state) => state.movie
+  );
+  //console.log(popularMovie, topRatedMovie, upComingMovie);
+  useEffect(() => {
+    dispatch(movieAction.getMovies());
+  }, []);
+  return (
+    <div>
+      {popularMovie.results && <Banner movie={popularMovie.results[0]} />}
+    </div>
+  );
+};
+export default Home;
