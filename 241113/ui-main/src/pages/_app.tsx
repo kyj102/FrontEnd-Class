4e179f153551a35fa306f75d3befa73dc80e536b
kyj102 <yain4746@naver.com>
2024-11-14 17:36:07 +0900

    자료구조 및 Next.js

--- 241113/ui-main/src/pages/_app.tsx
+++ 241113/ui-main/src/pages/_app.tsx
@@ -1,36 +1,25 @@
 import "@/styles/globals.css";
+import { ReactNode } from "react";
+import { NextPage } from "next";
 import type { AppProps } from "next/app";
-import Link from "next/link";
-import { useRouter } from "next/router";
+import GlobalLayout from "@/components/global-layout";
 
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactNode) => ReactNode;
+}
 
-// ui 설정 세팅
+// ui 설정세팅
 // component, pageProps 라는 두개의 매개변수를 인자값으로 받음
 // 자식요소에 페이지로 들어온 요소 = component
 // 자식요소의 컴포넌트들은 각각의 props를 받는다 = pageProps
-export default function App(
-  { Component, pageProps }
-  : AppProps) {
-  const router = useRouter();
-  const onClickButton = () => {
-    router.push("/test");
-  };
-  
+export default function App({ 
+  Component, 
+  pageProps,
+  }: AppProps & {Component: NextPageWithLayout}) {
+  const getLayout = Component.getLayout ?? ((page: ReactNode) => page);
   return (
-    <>
-    <header>
-      <Link href={"/"}>Index</Link>
-      &nbsp;
-      <Link href={"/search"}>Search</Link>
-      &nbsp;
-      <Link href={"/book/1"}>Book/1</Link>
-      <div>
-        <button onClick={onClickButton}>
-          /test 페이지로 이동
-        </button>
-      </div>
-    </header>
-    <Component {...pageProps} />
-    </>
+    <GlobalLayout>
+        {getLayout(<Component {...pageProps} />)}
+      </GlobalLayout>
   )
 }
--- 241113/ui-main/src/pages/index.tsx
+++ 241113/ui-main/src/pages/index.tsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
+import style from "./index.module.css";
+import SearchableLayout from '@/components/searchable-layout';
+import BookItem from '@/components/book-item';
+import books from "@/mock/book.json";
 
-const index = () => {
+const Home = () => {
   return (
-    <h1>
-      Index
-    </h1>
-  )
+    <main className={style.container}>
+      <section>
+        <h3>지금 추천하는 도서</h3>
+        {books.map((book) => (
+          <BookItem key={book.id} {...book} />
+          ))}
+      </section>
+      <section>
+        <h3>등록된 모든 도서</h3>
+        {books.map((book) => (
+          <BookItem key={book.id} {...book} />
+          ))}
+      </section>
+    </main>
+  );
+};
+
+// index페이지에서 특정한 레이아웃 사용 직접지정
+Home.getLayout = (page: ReactNode) => {
+  return <SearchableLayout>{page}</SearchableLayout>
 }
 
-export default index
+export default Home
